3ad67e2437e5d20f09d8028484e9d78ada1034c8
Kruno <kmposlovni@gmail.com>
2025-12-06 22:54:46 +0100

    Test

--- src/main/java/test/hr/dreactor/DreactorApplication.java
+++ src/main/java/test/hr/dreactor/DreactorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DreactorApplication {
 	public static void main(String[] args) {
-        System.out.println("Small change (2)");
+        System.out.println("Will this trigger image build?");
 		SpringApplication.run(DreactorApplication.class, args);
 	}
 }
